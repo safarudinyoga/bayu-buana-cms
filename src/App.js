b0ed277f3d628d8df868925a71b131103a08b5c9
robbybenta <robbybenta7@gmail.com>
2021-09-22 11:52:05 +0700

    initial frame dashboard, create componen button, breadcrumb, searchbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import { Helmet } from 'react-helmet';
+import './assets/css/styles.css';
 import Login from './views/Begin/Login';
-
-import { Provider } from 'react-redux'
-import store from './redux/stores'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
+import Home from './views/Home';
+import { Provider } from 'react-redux';
+import store from './redux/stores';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
-  return (
-    <Provider store={store}>
-      <Router basename="/dashboard">
-        <Switch>
-          <Route exact path ="/" component={Login} />
-        </Switch>
-      </Router>
-    </Provider >
-  );
+	return (
+		<>
+			<Helmet>
+				<link
+					href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+					rel="stylesheet"
+					integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+					crossorigin="anonymous"
+				/>
+				<script
+					src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+					integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+					crossorigin="anonymous"
+				></script>
+			</Helmet>
+			<Provider store={store}>
+				<Router basename="/dashboard">
+					<Switch>
+						<Route exact path="/" component={Login} />
+						<Route exact path="/Home" component={Home} />
+					</Switch>
+				</Router>
+			</Provider>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/atoms/Breadcrumb/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../Button/index';
+import { useLocation } from 'react-router-dom';
+export default function BreadCrummb({ data }) {
+	const location = useLocation();
+	return (
+		<>
+			<nav aria-label="breadcrumb">
+				<ol className="breadcrumb">
+					{data.map((item, index) => {
+						return (
+							<li
+								key={`breadcrumb-${index}`}
+								className={`breadcrumb-item ${
+									location.pathname === item.pageHref ? 'active' : ''
+								}`}
+							>
+								{location.pathname === item.pageHref ? (
+									item.pageTitle
+								) : (
+									<Button type="link" href={item.pageHref}>
+										{item.pageTitle}
+									</Button>
+								)}
+							</li>
+						);
+					})}
+				</ol>
+			</nav>
+		</>
+	);
+}
