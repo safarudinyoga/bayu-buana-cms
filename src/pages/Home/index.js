9415e13b2aa2a0c3730f01691595b395661a41b0
Dimas Bagus Susilo <dimasbagussusilo@gmail.com>
2021-09-27 00:11:56 +0700

    add prettier config

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import Content from '../../components/Molecules/Content.jsx';
+import Layout from '../../components/Organisms/Layout/index';
+import React from 'react';
+
+export default function index() {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
+}
