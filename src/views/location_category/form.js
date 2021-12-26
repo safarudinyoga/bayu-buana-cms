f7cac01b5bb22e763efedf790b19a44a006e11f1
irfanfath <mirfanf7@gmail.com>
2021-12-26 13:22:08 +0700

    Update form.js

--- src/views/location_category/form.js
+++ src/views/location_category/form.js
@@ -6,13 +6,16 @@ import FormInputControl from "components/form/input-control"
 import FormBuilder from "components/form/builder"
 import useQuery from "lib/query"
 import {useDispatch} from "react-redux"
-import {setUIParams} from "redux/ui-store"
+import {setAlert, setUIParams} from "redux/ui-store"
+import $ from "jquery"
+import env from "../../config/environment"
 
 const endpoint = "/master/location-categories"
 const backUrl = "/master/location-categories"
 
 function LocationCategoryForm(props) {
   let dispatch = useDispatch()
+  let formId = props.match.params.id
 
   const isView = useQuery().get("action") === "view"
   const [formBuilder, setFormBuilder] = useState(null)
@@ -34,13 +37,28 @@ function LocationCategoryForm(props) {
   const validationRules = {
     location_category_code: {
       required: true,
-      min: 0,
+      min: 1,
       max: 99,
+      checkCode: true,
     },
     location_category_name: {
       required: true,
       minlength: 1,
       maxlength: 256,
+      checkName: true
+    },
+  }
+
+  const validationMessages = {
+    location_category_name: {
+      required: "Location category Name is required",
+      minlength: "Location category Name must be at least 1 characters",
+      maxlength: "Location category Name cannot be more than 99 characters",
+    },
+    location_category_code: {
+      required: "Location category Code is required",
+      minlength: "Location category Code must be at least 1 characters",
+      maxlength: "Location category Code cannot be more than 36 characters",
     },
   }
 
@@ -76,6 +94,62 @@ function LocationCategoryForm(props) {
       try {
         let res = await api.get(endpoint + "/" + formId)
         setForm(res.data)
+
+        if(res.data){
+          let currentCode = res.data.location_category_code
+          let currentName = res.data.location_category_name
+
+          $.validator.addMethod(
+            "checkCode",
+            function (value, element) {
+              var req = false
+              $.ajax({
+                type: "GET",
+                async: false,
+                url: `${env.API_URL}/master/location-categories?filters=["location_category_code","=","${element.value}"]`,
+                success: function (res) {
+                  if (res.items.length !== 0) {
+                    if(currentCode == element.value){
+                      req = true
+                    } else {
+                      req = false
+                    }
+                  } else {
+                    req = true
+                  }
+                },
+              })
+    
+              return req
+            },
+            "Location Category Code already exists",
+          )
+          $.validator.addMethod(
+            "checkName",
+            function (value, element) {
+              var req = false
+              $.ajax({
+                type: "GET",
+                async: false,
+                url: `${env.API_URL}/master/location-categories?filters=["location_category_name","=","${element.value}"]`,
+                success: function (res) {
+                  if (res.items.length !== 0) {
+                    if(currentName.toUpperCase() === element.value.toUpperCase()){
+                      req = true
+                    } else {
+                      req = false
+                    }
+                  } else {
+                    req = true
+                  }
+                },
+              })
+    
+              return req
+            },
+            "Location Category Name already exists",
+          )
+        }
       } catch (e) { }
 
       try {
@@ -85,6 +159,49 @@ function LocationCategoryForm(props) {
         setTranslations(res.data.items)
       } catch (e) { }
       setLoading(false)
+    } else {
+      $.validator.addMethod(
+        "checkCode",
+        function (value, element) {
+          var req = false
+          $.ajax({
+            type: "GET",
+            async: false,
+            url: `${env.API_URL}/master/location-categories?filters=["location_category_code","=","${element.value}"]`,
+            success: function (res) {
+              if (res.items.length !== 0) {
+                req = false
+              } else {
+                req = true
+              }
+            },
+          })
+
+          return req
+        },
+        "Location Category Code already exists",
+      )
+      $.validator.addMethod(
+        "checkName",
+        function (value, element) {
+          var req = false
+          $.ajax({
+            type: "GET",
+            async: false,
+            url: `${env.API_URL}/master/location-categories?filters=["location_category_name","=","${element.value}"]`,
+            success: function (res) {
+              if (res.items.length !== 0) {
+                req = false
+              } else {
+                req = true
+              }
+            },
+          })
+
+          return req
+        },
+        "Location Category Name already exists",
+      )
     }
   }, [])
 
@@ -108,9 +225,19 @@ function LocationCategoryForm(props) {
         await api.putOrPost(path, tl.id, tl)
       }
     } catch (e) {
+      dispatch(
+        setAlert({
+          message: `Failed to ${formId ? "update" : "save"} this record.`,
+        }),
+      )
     } finally {
       setLoading(false)
       props.history.push(backUrl)
+      dispatch(
+        setAlert({
+          message: `Record ${form.trip_type_code} - ${form.trip_type_name} has been successfully ${formId ? "updated" : "saved"}..`,
+        }),
+      )
     }
   }
 
@@ -125,6 +252,7 @@ function LocationCategoryForm(props) {
       alertMessage={"Incomplete data"}
       isValid={false}
       rules={validationRules}
+      validationMessages={validationMessages}
     >
       <FormHorizontal>
         <FormInputControl
