27b33c946c33e15dd515aee2f209b17eef221bd1
bacing88 <bacing88@gmail.com>
2021-12-30 17:25:21 +0700

    fix

--- src/components/form/input-wrapper.js
+++ src/components/form/input-wrapper.js
@@ -1,4 +1,5 @@
 import { Component } from "react"
+import Hints from "assets/icons/hint.svg"
 
 export default class FormInputWrapper extends Component {
   render() {
@@ -7,8 +8,8 @@ export default class FormInputWrapper extends Component {
       <div className="form-group required row d-flex align-items-center">
         <span className={`text-label-input col-sm-${cl?.sm||6} col-md-${cl?.md||5} col-lg-${cl?.lg||4} col-form-label ${this.props.required ? "label-required" : ""}`}>
         {this.props.label}
-        {this.props.hint ? <i className="fa fa-info-circle hint" title={this.props.hint}></i>: ""}
-        </span>
+        {this.props.hint ? <img src={Hints} alt="hint" className="ml-1 mb-2" title={this.props.hint}/>: ""}        
+        </span>        
         <div className={`form-control-wrapper col-sm-${cl?.sm ? 12 - cl.sm : 6} col-md-${cl?.md||7} col-lg-${cl?.lg ? 12 - cl.lg : 8}`}>{this.props.children}</div>
       </div>
     )
