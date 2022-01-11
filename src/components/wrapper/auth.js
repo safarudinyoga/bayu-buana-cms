88db8b676114361bdfcdd8ab4559d0bef5bf81d5
annisa <annisaayu569@gmail.com>
2022-01-11 13:36:25 +0700

    logo white

--- src/components/wrapper/auth.js
+++ src/components/wrapper/auth.js
@@ -25,51 +25,12 @@ const AuthWrapper = (props) => {
 					</div>
 					<div className="card-form-body">
 						{props.children}
-						{/* <p className="title p-0 mb-1">Welcome Back!</p>
-						<p className="sub-title p-0 mb-5">Please Sign in to continue</p>
-
-						<Form.Label>Email</Form.Label>
-						<Form.Control
-							value={form.email}
-							name="email"
-							onChange={(e) => setForm({ ...form, email: e.target.value })}
-							disabled={false}
-							type="email"
-						/>
-
-						<Form.Label className="mt-2">Password</Form.Label>
-						<Form.Control
-							value={form.password}
-							name="password"
-							onChange={(e) => setForm({ ...form, password: e.target.value })}
-							disabled={false}
-							type="password"
-						/>
-
-						<div className="row mt-2">
-							<div className="col"> 
-								<input 
-									type="checkbox" 
-									checked={rememberVal}
-									onClick={() => setCheckBox(!rememberVal)}
-								/>
-								<span onClick={() => setCheckBox(!rememberVal)} className="remember pl-1">
-									Remember Me
-								</span>
-							</div>
-							<div className="col text-right">
-								<a className="forgot-text">Forgot Password?</a>
-							</div>
-						</div> */}
 
 						<BlockButton text="SIGN IN" onClick={() => {}}/>
-
-						{/* <button className=" btn btn-block button-save mt-4">
-							<div className="button-save-text">SIGN IN</div>
-						</button> */}
 					</div>
 					<div className="card-form-footer position-absolute pb-4">
-						<img src="/img/logo.png" className="bb-logo" alt="logo"/>
+						<img src="/img/logo.png" className="bb-logo d-none d-md-block" alt="logo"/>
+						<img src="/img/logo_w.png" className="bb-logo d-md-none" alt="logo"/>
 						&copy; 2021 Bayu Buana Travel Services. All Rights Reserved.
 					</div>
 				</div>
