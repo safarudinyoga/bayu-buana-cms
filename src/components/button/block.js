7d74e76b200664104601e1d05a3697fc49723770
annisa <annisaayu569@gmail.com>
2022-01-17 11:31:02 +0700

    add props for block button

--- src/components/button/block.js
+++ src/components/button/block.js
@@ -1,13 +1,20 @@
 import "./button.css"
 import React from "react"
 
-export const BlockButton = (props) => {
+export const BlockButton = ({
+    onClick = () => {},
+    text = "sign in",
+    disabled =  false,
+    type = "button"
+}) => {
     return (
     <button 
         className=" btn btn-block button-save mt-4"
-        onClick={props.onClick}
+        onClick={onClick}
+        disabled={disabled}
+        type={type}
     >
-        <div className="text-button-block">{props.text}</div>
+        <div className="text-button-block">{text}</div>
     </button>
     )
 }
