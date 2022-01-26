8545d7c537af9c66b4e2df1da0be188b02edfa6c
annisaayu <annisaayu569@gmail.com>
2022-01-26 16:19:29 +0700

    delete message

--- src/components/table/bb-data-table.js
+++ src/components/table/bb-data-table.js
@@ -40,6 +40,7 @@ class BBDataTable extends Component {
       status: "0",
       extraFilters: this.props.filters || [],
       isOpen: false,
+      itemInfo: ""
     }
     this.inProgress = false
 
@@ -82,7 +83,8 @@ class BBDataTable extends Component {
     } catch (e) {}
 
     const allowed = [this.props.recordName]
-    const { recordName } = this.props
+    const { recordName, msgType } = this.props
+    const self = this
     columns.push({
       searchable: false,
       orderable: false,
@@ -112,7 +114,12 @@ class BBDataTable extends Component {
                 .join(" - ")
             : row[recordName]
           : ""
-
+        console.log(display, row)
+        let itemInfo = msgType === "colon"
+          ? `'${recordName[0].split("_").join(" ")}: ${row[recordName[0]]}
+          ${recordName[1].split("_").join(" ")}: ${row[recordName[1]]}'`
+          : cvtRecordName
+        self.setState({ itemInfo })
         var x = window.matchMedia("(max-width: 768px)")
         tooltipCust(x)
         x.addListener(tooltipCust)
@@ -842,7 +849,9 @@ class BBDataTable extends Component {
                 : this.props.title
               : this.props.title}
           </ModalHeader>
-          <ModalBody>Are you sure you want to delete this?</ModalBody>
+          <ModalBody>Are you sure you want to delete {
+            this.props.showInfoDelete ? this.state.itemInfo : "this"
+          }?</ModalBody>
           <ModalFooter>
             <Button
               variant="danger"
@@ -855,7 +864,7 @@ class BBDataTable extends Component {
                     .delete(this.props.endpoint + "/" + this.state.id)
                     .then(() => {
                       this.props.setAlert({
-                        message: `Record ${this.state.name} was successfully deleted.`,
+                        message: `Record ${this.state.itemInfo} was successfully deleted.`,
                       })
                     })
                     .catch(function (error) {
