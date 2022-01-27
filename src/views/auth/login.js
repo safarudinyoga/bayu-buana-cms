fb3f051b04b4f40ce9354d58aab1a8d928ff732a
annisa <annisaayu569@gmail.com>
2022-01-27 15:01:42 +0700

    remove cookies after 30 seconds

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -55,7 +55,7 @@ function Login() {
 			let res = await api.post("/user/login", values)
 
 			let date = new Date();
-			date.setTime(date.getTime() + (res.data.expires_in));
+			date.setTime(date.getTime() + (res.data.expires_in * 10));
 			Cookies.set('ut', res.data.access_token, {expires: date})
 
 			if (rememberMe) {
