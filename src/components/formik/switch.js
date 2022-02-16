0f14f76d7cb7931196050454cc8d83aa4a3ce63e
annisa <annisaayu569@gmail.com>
2022-02-16 15:30:05 +0700

    add react switch

---
+++ src/components/formik/switch.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Field } from "formik"
+import { Row, Col } from "react-bootstrap"
+import Switch from "react-switch"
+
+function inputForm(props) {
+  const {
+    label,
+    name,
+    value,
+    size,
+    onChange
+  } = props
+  return (
+    <Row className="form-group">
+      <Col md={size?.label?.md || 3} lg={size?.label?.lg || 3}>
+        <label className="text-label-input" htmlFor={name}>
+          {label}
+        </label>
+      </Col>
+      <Col  md={size?.value?.md || 9} lg={size?.value?.lg || 9}>
+        <div style={{display: 'inline', verticalAlign: "middle"}}>
+          <Switch 
+            onChange={onChange} 
+            checked={value || false} 
+            checkedIcon={false}
+            uncheckedIcon={false}
+            width={27}
+            height={15}
+          />
+        </div>
+      </Col>
+    </Row>
+  )
+}
+
+export default inputForm
