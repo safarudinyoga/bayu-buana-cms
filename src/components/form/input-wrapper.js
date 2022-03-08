5104ec4475c83ebcf6d80f34c629640b5e070e4b
Annisa_Ayu_Pertiwi <annisaayu569@gmail.com>
2022-03-08 13:34:09 +0700

    change form width

--- src/components/form/input-wrapper.js
+++ src/components/form/input-wrapper.js
@@ -3,7 +3,8 @@ import Hints from "assets/icons/hint.svg"
 
 export default class FormInputWrapper extends Component {
   render() {
-    const { cl, notes } = this.props
+    const { cl, notes, cr } = this.props
+    let col_form_md= cr ? cr.md : cl ? cl.md : 7
     return (
       <div className="form-group required row d-flex align-items-center">
         <div className={`col-sm-${cl?.sm||6} col-md-${cl?.md||5} col-lg-${cl?.lg||4} col-form-label`}>
@@ -26,7 +27,7 @@ export default class FormInputWrapper extends Component {
             : null
           }
         </div>
-        <div className={`form-control-wrapper col-sm-${cl?.sm ? 12 - cl.sm : 6} col-md-${cl?.md||7} col-lg-${cl?.lg ? 12 - cl.lg : 8}`}>{this.props.children}</div>
+        <div className={`form-control-wrapper col-sm-${cl?.sm ? 12 - cl.sm : 6} col-md-${col_form_md} col-lg-${cl?.lg ? 12 - cl.lg : 8}`}>{this.props.children}</div>
       </div>
     )
   }
