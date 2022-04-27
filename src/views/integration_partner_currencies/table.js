61a6f525aba9fddb3b57e716d6369afec278795f
irfanfath <mirfanf7@gmail.com>
2022-04-27 16:55:19 +0700

    add integration partner currencies

---
+++ src/views/integration_partner_currencies/table.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react"
+import BBDataTable from "components/table/bb-data-table"
+import { useDispatch } from "react-redux"
+import { setUIParams } from "redux/ui-store"
+import Form from "./form"
+import { Card } from "react-bootstrap"
+
+export default function IntegrationPartnerCurrenciesTable() {
+  let dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(
+      setUIParams({
+        title: "Partner Corporate",
+        breadcrumbs: [
+          {
+            text: "Partner Corporate",
+          },
+        ],
+      }),
+    )
+  }, [])
+
+
+  let params = {
+    isCheckbox: false,
+    showAdvancedOptions: false,
+    createOnModal: true,
+    hideDetail: true,
+    title: "Partner Currencies",
+    titleModal: "Partner Currencies",
+    baseRoute: "/master/integration-partner-currencies/form",
+    endpoint: "/master/integration-partner-currencies",
+    deleteEndpoint: "/master/batch-actions/delete/integration-partner-currencies",
+    columns: [
+      {
+        title: "Currency",
+        data: "currency_symbol",
+      },
+      {
+        title: "Partner Currency Code",
+        data: "currency_code",
+      },
+      {
+        title: "Partner Currency Name",
+        data: "currency_name",
+      },
+    ],
+    emptyTable: "No Partner Currency found",
+    recordName: ["currency.currency_name", "currency_code", "currency_name"],
+    btnDownload: ".buttons-csv",
+    module: "integration-partner-currencies"
+  }
+  return (
+    <Card style={{marginBottom: 0}}>
+        <Card.Body className="px-1 px-md-4">
+          <h3 className="card-heading">Partner Currencies</h3>
+          <BBDataTable {...params} modalContent={Form} />
+        </Card.Body>
+      </Card>
+  ) 
+}
