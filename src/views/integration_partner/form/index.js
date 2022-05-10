fe8f71be1962b5a21a48faea2413ae89703ffb6b
sony <sonyfebrian362@gmail.com>
2022-05-10 10:04:34 +0700

    fix-conflict

--- src/views/integration_partner/form/index.js
+++ src/views/integration_partner/form/index.js
@@ -1,21 +1,17 @@
 import { withRouter, useHistory } from "react-router"
 import React, { useEffect, useState } from "react"
 import Api from "config/api"
-import { useDispatch } from 'react-redux';
-import { withRouter} from 'react-router';
+import { useDispatch } from "react-redux"
 import { ReactSVG } from "react-svg"
 import { Row, Col, Tab, Nav } from "react-bootstrap"
 import useQuery from "lib/query"
-import { useDispatch } from "react-redux"
 import { setUIParams } from "redux/ui-store"
 import PartnerPaymentGateway from "./partner_payment_gateway/table"
 import PartnerMealPlans from "./partner_meal_plans/table"
 import PartnertMessages from "./partner_messages/table"
-import Api from "config/api"
 import { useSnackbar } from "react-simple-snackbar"
 import PartnerCabin from "../../integration_partner_cabin/tabel"
 import PartnerInformation from "./partner-information"
-
 
 const endpoint = "/master/integration-partners"
 const backUrl = "/master/integration-partner"
@@ -41,7 +37,6 @@ const IntegrationPartnerForm = (props) => {
   const [tabKey, setTabKey] = useState("partner-information")
   const [loading, setLoading] = useState(true)
   const [Data, setData] = useState(null)
-  
 
   useEffect(async () => {
     let api = new Api()
@@ -91,110 +86,102 @@ const IntegrationPartnerForm = (props) => {
 
   return (
     <>
-     
-          <Tab.Container activeKey={tabKey} onSelect={handleSelectTab}>
-            <Row>
-              <Col sm={3}>
-                <Nav variant="pills" className="flex-column nav-side">
-                  <Nav.Item>
-                    <Nav.Link eventKey="partner-information">
-                      <div>
-                        <ReactSVG src="/img/icons/users.svg" />
-                        <span>Partner Information</span>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="partner-credential">
-                      <div>
-                        <ReactSVG src="/img/icons/users.svg" />
-                        <span>Partner Credential</span>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="partner-corporates">
-                      <div>
-                        <ReactSVG src="/img/icons/users.svg" />
-                        <span>Partner Corporates</span>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                  <Nav.Link href={`/master/integration-partner-cabin-types`}>
-                      <div>
-                        <ReactSVG src="/img/icons/employment.svg" />
-                        <span>Partner Cabins</span>
-                      </div>
-                    </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                    <Nav.Link eventKey="partner-meal-plans">
-                      <div>
-                        <ReactSVG src="/img/icons/users.svg" />
-                        <span>Partner Meal Plans</span>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="partner-fee-taxes">
-                      <div>
-                        <ReactSVG src="/img/icons/users.svg" />
-                        <span>Partner Fee Taxes</span>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="partner-messages">
-                      <div>
-                        <ReactSVG src="/img/icons/users.svg" />
-                        <span>Partner Messages</span>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  
-                  {/* <Nav.Link href={`/master/integration-partner-cities/${formId}/cities`}>
+      <Tab.Container activeKey={tabKey} onSelect={handleSelectTab}>
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column nav-side">
+              <Nav.Item>
+                <Nav.Link eventKey="partner-information">
+                  <div>
+                    <ReactSVG src="/img/icons/users.svg" />
+                    <span>Partner Information</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="partner-credential">
+                  <div>
+                    <ReactSVG src="/img/icons/users.svg" />
+                    <span>Partner Credential</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="partner-corporates">
+                  <div>
+                    <ReactSVG src="/img/icons/users.svg" />
+                    <span>Partner Corporates</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href={`/master/integration-partner-cabin-types`}>
+                  <div>
+                    <ReactSVG src="/img/icons/employment.svg" />
+                    <span>Partner Cabins</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="partner-meal-plans">
+                  <div>
+                    <ReactSVG src="/img/icons/users.svg" />
+                    <span>Partner Meal Plans</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="partner-fee-taxes">
+                  <div>
+                    <ReactSVG src="/img/icons/users.svg" />
+                    <span>Partner Fee Taxes</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="partner-messages">
+                  <div>
+                    <ReactSVG src="/img/icons/users.svg" />
+                    <span>Partner Messages</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+
+              {/* <Nav.Link href={`/master/integration-partner-cities/${formId}/cities`}>
                     <div>
                         <ReactSVG src="/img/icons/employment.svg" />
                         <span>Partner City</span>
                       </div>
                     </Nav.Link> */}
-                   
-                  
-                </Nav>
-              </Col>
-              <Col sm={9}>
-                <Tab.Content>
-                  <Tab.Pane eventKey="partner-information">
-                    <PartnerInformation />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="partner-cabins">
-                    <PartnerCabin />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="partner-credential">
-                    Partner Credential
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="partner-corporates">
-                    Partner Corporates
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="partner-meal-plans">
-                    Partner Meal Plans
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="partner-fee-taxes">
-                    Partner Fee Taxes
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="partner-messages">
-                    Partner Messages
-                  </Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
-       
-      
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="partner-information">
+                <PartnerInformation />
+              </Tab.Pane>
+              <Tab.Pane eventKey="partner-cabins">
+                <PartnerCabin />
+              </Tab.Pane>
+              <Tab.Pane eventKey="partner-credential">
+                Partner Credential
+              </Tab.Pane>
+              <Tab.Pane eventKey="partner-corporates">
+                Partner Corporates
+              </Tab.Pane>
+              <Tab.Pane eventKey="partner-meal-plans">
+                Partner Meal Plans
+              </Tab.Pane>
+              <Tab.Pane eventKey="partner-fee-taxes">
+                Partner Fee Taxes
+              </Tab.Pane>
+              <Tab.Pane eventKey="partner-messages">Partner Messages</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </>
-    
-  );
+  )
 }
 
 export default withRouter(IntegrationPartnerForm)
--- src/views/integration_partner_city/form.js
+++ src/views/integration_partner_city/form.js
@@ -1,8 +1,8 @@
 import { withRouter } from "react-router"
 import React, { useState, useEffect } from "react"
-import { Col, Form, Row, Button} from 'react-bootstrap';
+import { Col, Form, Row, Button } from "react-bootstrap"
 import { Formik, FastField } from "formik"
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid"
 import * as Yup from "yup"
 import SelectAsync from "components/form/select-async"
 import Api from "config/api"
@@ -14,7 +14,6 @@ import CancelButton from "components/button/cancel"
 
 const endpoint = "/master/integration-partner-cities"
 
-
 function PartnerCityForm(props) {
   const dispatch = useDispatch()
   const showCreateModal = useSelector((state) => state.ui.showCreateModal)
@@ -23,10 +22,6 @@ function PartnerCityForm(props) {
   const [id, setId] = useState(null)
   const [loading, setLoading] = useState(true)
   const [formValues, setFormValues] = useState(null)
-  
-  
-
-
 
   useEffect(async () => {
     let formId = showCreateModal.id || props.id
@@ -34,10 +29,9 @@ function PartnerCityForm(props) {
     let docTitle = "Edit Partner Cities"
     if (!formId) {
       docTitle = "New Partner Cities"
-    } 
+    }
 
     dispatch(setModalTitle(docTitle))
-
 
     if (formId) {
       try {
@@ -67,54 +61,48 @@ function PartnerCityForm(props) {
     city_name: "",
   }
 
-
-
   const validationSchema = Yup.object().shape({
-    city: Yup.object()
-      .required("City is required.."),
-    city_code:Yup.string()
-      .required("Partner City Code is required")
-      .test(
-        "Unique Partner City Code",
-        "Partner City Code already exists", // <- key, message
-        async (value, ctx) => {
-          let formId = props.match.params.id
-          try {
-            let res = await axios.get(`${env.API_URL}/master/integration-partner-cities?filters=["city_code","=","${value}"]`)
-  
-            if (formId) {
-              return res.data.items.length === 0 ||
-                value === formValues.city_code
-            } else {
-              return res.data.items.length === 0
-            }
-          } catch (e) {
-            return false
-          }
-        }
-      ),
-    city_name: Yup.string()
-      .required("Partner City Name is required")
-      .test(
-        "Unique Partner City Name",
-        "Partner City Name already exists", // <- key, message
-        async (value, ctx) => {
-          let formId = props.match.params.id
-          try {
-            let res = await axios.get(`${env.API_URL}/master/integration-partner-cities?filters=["city_name","=","${value}"]`)
-  
-            if (formId) {
-              return res.data.items.length === 0 ||
-                value === formValues.city_name
-            } else {
-              return res.data.items.length === 0
-            }
-          } catch (e) {
-            return false
-          }
-        }
-      ),
+    city: Yup.object().required("City is required.."),
+    city_code: Yup.string().required("Partner City Code is required"),
+    // .test(
+    //   "Unique Partner City Code",
+    //   "Partner City Code already exists", // <- key, message
+    //   async (value, ctx) => {
+    //     let formId = props.match.params.id
+    //     try {
+    //       let res = await axios.get(`${env.API_URL}/master/integration-partner-cities?filters=["city_code","=","${value}"]`)
 
+    //       if (formId) {
+    //         return res.data.items.length === 0 ||
+    //           value === formValues.city_code
+    //       } else {
+    //         return res.data.items.length === 0
+    //       }
+    //     } catch (e) {
+    //       return false
+    //     }
+    //   }
+    // ),
+    city_name: Yup.string().required("Partner City Name is required"),
+    // .test(
+    //   "Unique Partner City Name",
+    //   "Partner City Name already exists", // <- key, message
+    //   async (value, ctx) => {
+    //     let formId = props.match.params.id
+    //     try {
+    //       let res = await axios.get(`${env.API_URL}/master/integration-partner-cities?filters=["city_name","=","${value}"]`)
+
+    //       if (formId) {
+    //         return res.data.items.length === 0 ||
+    //           value === formValues.city_name
+    //       } else {
+    //         return res.data.items.length === 0
+    //       }
+    //     } catch (e) {
+    //       return false
+    //     }
+    //   }
+    // ),
   })
 
   const onSubmit = async (values, a) => {
@@ -124,23 +112,31 @@ function PartnerCityForm(props) {
         city_id: uuidv4(),
         city_code: values.city_code,
         city_name: values.city_name,
-
       }
-
 
       if (!formId) {
         //Proses Create Data
         let integration_parter_id = props.match.params.id
-        let res = await API.post(`/master/integration-partners/${integration_parter_id}/cities`, form)
-        dispatch(setCreateModal({ show: false, id: null, disabled_form: false }))
+        let res = await API.post(
+          `/master/integration-partners/${integration_parter_id}/cities`,
+          form,
+        )
+        dispatch(
+          setCreateModal({ show: false, id: null, disabled_form: false }),
+        )
         dispatch(
           setAlert({
             message: `Record 'Partner City Name: ${form.city_name}' has been successfully saved.`,
           }),
         )
       } else {
-        let res = await API.put(`/master/integration-partner-cities/${formId}`, form)
-        dispatch(setCreateModal({ show: false, id: null, disabled_form: false }))
+        let res = await API.put(
+          `/master/integration-partner-cities/${formId}`,
+          form,
+        )
+        dispatch(
+          setCreateModal({ show: false, id: null, disabled_form: false }),
+        )
         dispatch(
           setAlert({
             message: `Record 'Partner City Name: ${form.city_name}' has been successfully saved.`,
@@ -155,7 +151,7 @@ function PartnerCityForm(props) {
       )
     }
   }
- 
+
   return (
     <Formik
       initialValues={formValues || initialValues}
@@ -164,158 +160,156 @@ function PartnerCityForm(props) {
       validateOnMount
       enableReinitialize
     >
-      {
-        ({
-          values,
-          errors,
-          touched,
-          dirty,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          isValid,
-          setFieldValue,
-          setFieldTouched,
-        }) => (
-          <Form onSubmit={handleSubmit} className="ml-2">
-            <Form.Group as={Row} className="form-group">
-              <Form.Label column sm={4}>
-                City<span className="form-label-required">*</span>
-              </Form.Label>
-              <Col sm={8}>
-                <FastField name="city">
-                  {({ field, form }) => (
-                    <div style={{ maxWidth: 200 }}>
-                      <SelectAsync
-                        {...field}
-                        isClearable
-                        isDisabled={isView}
-                        url={`master/cities`}
-                        fieldName="city_name"
-                        onChange={(v) => {
-                          setFieldValue("city", v)
-                        }}
-                        placeholder="Please choose"
-                        className={`react-select ${form.touched.city &&
-                            form.errors.city
-                            ? "is-invalid"
-                            : null
-                          }`}
-                        components={
-                          isView
-                            ? {
+      {({
+        values,
+        errors,
+        touched,
+        dirty,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        isValid,
+        setFieldValue,
+        setFieldTouched,
+      }) => (
+        <Form onSubmit={handleSubmit} className="ml-2">
+          <Form.Group as={Row} className="form-group">
+            <Form.Label column sm={4}>
+              City<span className="form-label-required">*</span>
+            </Form.Label>
+            <Col sm={8}>
+              <FastField name="city">
+                {({ field, form }) => (
+                  <div style={{ maxWidth: 200 }}>
+                    <SelectAsync
+                      {...field}
+                      isClearable
+                      isDisabled={isView}
+                      url={`master/cities`}
+                      fieldName="city_name"
+                      onChange={(v) => {
+                        setFieldValue("city", v)
+                      }}
+                      placeholder="Please choose"
+                      className={`react-select ${
+                        form.touched.city && form.errors.city
+                          ? "is-invalid"
+                          : null
+                      }`}
+                      components={
+                        isView
+                          ? {
                               DropdownIndicator: () => null,
                               IndicatorSeparator: () => null,
                             }
-                            : null
-                        }
-                      />
-                      {form.touched.city &&
-                        form.errors.city && (
-                          <Form.Control.Feedback type="invalid">
-                            {form.touched.city
-                              ? form.errors.city
-                              : null}
-                          </Form.Control.Feedback>
-                        )}
-                    </div>
-                  )}
-                </FastField>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="form-group">
-              <Form.Label column md={3} lg={4}>
-                Partner City Code{" "}
-                <span className="form-label-required">*</span>
-              </Form.Label>
-              <Col md={9} lg={8}>
-                <FastField name="city_code" disabled>
-                  {({ field, form }) => (
-                    <>
-                      <Form.Control
-                        type="text"
-                        disabled={isView}
-                        isInvalid={
-                          form.touched.city_code &&
-                          form.errors.city_code
-                        }
-                        minLength={1}
-                        maxLength={128}
-                        {...field}
-                        style={{ maxWidth: 300 }}
-                      />
-                      {form.touched.city_code &&
-                        form.errors.city_code && (
-                          <Form.Control.Feedback type="invalid">
-                            {form.touched.city_code
-                              ? form.errors.city_code
-                              : null}
-                          </Form.Control.Feedback>
-                        )}
-                    </>
-                  )}
-                </FastField>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="form-group">
-              <Form.Label column md={3} lg={4}>
-                Partner City Name{" "}
-                <span className="form-label-required">*</span>
-              </Form.Label>
-              <Col md={9} lg={8}>
-                <FastField name="city_name" disabled>
-                  {({ field, form }) => (
-                    <>
-                      <Form.Control
-                        type="text"
-                        disabled={isView}
-                        isInvalid={
-                          form.touched.city_name &&
-                          form.errors.city_name
-                        }
-                        minLength={1}
-                        maxLength={128}
-                        {...field}
-                        style={{ maxWidth: 300 }}
-                      />
-                      {form.touched.city_name &&
-                        form.errors.city_name && (
-                          <Form.Control.Feedback type="invalid">
-                            {form.touched.city_name
-                              ? form.errors.city_name
-                              : null}
-                          </Form.Control.Feedback>
-                        )}
-                    </>
-                  )}
-                </FastField>
-              </Col>
-            </Form.Group>
-           
-            {!props.hideButton && <div
+                          : null
+                      }
+                    />
+                    {form.touched.city && form.errors.city && (
+                      <Form.Control.Feedback type="invalid">
+                        {form.touched.city ? form.errors.city : null}
+                      </Form.Control.Feedback>
+                    )}
+                  </div>
+                )}
+              </FastField>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="form-group">
+            <Form.Label column md={3} lg={4}>
+              Partner City Code <span className="form-label-required">*</span>
+            </Form.Label>
+            <Col md={9} lg={8}>
+              <FastField name="city_code" disabled>
+                {({ field, form }) => (
+                  <>
+                    <Form.Control
+                      type="text"
+                      disabled={isView}
+                      isInvalid={
+                        form.touched.city_code && form.errors.city_code
+                      }
+                      minLength={1}
+                      maxLength={128}
+                      {...field}
+                      style={{ maxWidth: 300 }}
+                    />
+                    {form.touched.city_code && form.errors.city_code && (
+                      <Form.Control.Feedback type="invalid">
+                        {form.touched.city_code ? form.errors.city_code : null}
+                      </Form.Control.Feedback>
+                    )}
+                  </>
+                )}
+              </FastField>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="form-group">
+            <Form.Label column md={3} lg={4}>
+              Partner City Name <span className="form-label-required">*</span>
+            </Form.Label>
+            <Col md={9} lg={8}>
+              <FastField name="city_name" disabled>
+                {({ field, form }) => (
+                  <>
+                    <Form.Control
+                      type="text"
+                      disabled={isView}
+                      isInvalid={
+                        form.touched.city_name && form.errors.city_name
+                      }
+                      minLength={1}
+                      maxLength={128}
+                      {...field}
+                      style={{ maxWidth: 300 }}
+                    />
+                    {form.touched.city_name && form.errors.city_name && (
+                      <Form.Control.Feedback type="invalid">
+                        {form.touched.city_name ? form.errors.city_name : null}
+                      </Form.Control.Feedback>
+                    )}
+                  </>
+                )}
+              </FastField>
+            </Col>
+          </Form.Group>
+
+          {!props.hideButton && (
+            <div
               style={{
                 marginBottom: 30,
                 marginTop: 30,
                 display: "flex",
               }}
             >
-              {!isView && <Button
-                variant="primary"
-                type="submit"
-                disabled={isSubmitting}
-                style={{ marginRight: 15 }}
-              >
-                SAVE
-              </Button>}
-              <CancelButton onClick={() => dispatch(setCreateModal({ show: false, id: null, disabled_form: false }))} />
-            </div>}
-          </Form>
-        )
-      }
-
+              {!isView && (
+                <Button
+                  variant="primary"
+                  type="submit"
+                  disabled={isSubmitting}
+                  style={{ marginRight: 15 }}
+                >
+                  SAVE
+                </Button>
+              )}
+              <CancelButton
+                onClick={() =>
+                  dispatch(
+                    setCreateModal({
+                      show: false,
+                      id: null,
+                      disabled_form: false,
+                    }),
+                  )
+                }
+              />
+            </div>
+          )}
+        </Form>
+      )}
     </Formik>
   )
 }
 
-export default withRouter(PartnerCityForm) 
+export default withRouter(PartnerCityForm)
