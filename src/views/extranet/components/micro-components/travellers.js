907809715bf7f661a27eb3b2a88fee0cd1006b31
root <hashfirasis91@gmail.com>
2022-05-17 13:49:36 +0700

    fix some special dates bug and book trip

--- src/views/extranet/components/micro-components/travellers.js
+++ src/views/extranet/components/micro-components/travellers.js
@@ -4,7 +4,7 @@ import { Form, Popover, OverlayTrigger, Button } from 'react-bootstrap'
 import Select from 'components/form/select';
 
 function Travellers(props) {
-  const [adultCount, setAdultCount] = useState(0)
+  const [adultCount, setAdultCount] = useState(1)
   const [childrenCount, setChildrenCount] = useState(0)
   const [infantCount, setInfantCount] = useState(0)
 
@@ -39,6 +39,72 @@ function Travellers(props) {
     document.body.click()
   }
 
+  const travelerAgeStyle = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px dashed #ccc',
+      color: state.isSelected ? 'white' : 'gray',
+      padding: 2,
+      fontSize: '20px',
+     }),
+     control: () => ({
+      // none of react-select's styles are passed to <Control />
+      width: 500,
+      background: 'white',
+      height: 50,
+      radius: 40,
+      borderRadius: 5,
+      display: 'flex', 
+     }),
+     singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+     
+      return { ...provided, opacity, transition };
+     }
+  }
+
+  const selectChildAge = () => {
+    const options = []
+  
+    for (let i = 2; i <= 17; i++) {
+      options.push({
+        value: i,
+        label: i,
+      })
+    }
+  
+    return options
+  }
+
+  const infantAgeOptions = [
+    {
+      value: 0,
+      label: "Under 1",
+    },
+    {
+      value: 1,
+      label: 1,
+    }
+  ]
+
+  function ChildrenDiv(){
+    let divs = []
+
+    for (let i = 0; i < childrenCount; i++) {
+      divs.push(
+        <div className='col-4'>
+          <span className='traveler-title-detail'>Child {i+1} age</span>
+          <Select
+            options={selectChildAge()}
+            styles={travelerAgeStyle}
+          />
+        </div>
+      )
+    }
+    return divs
+  }
+
   const popover = (
     <Popover id="passenger-popover" className='passenger-popover'>
       <Popover.Content>
@@ -46,61 +112,71 @@ function Travellers(props) {
           <div className='col-md-6'>ADULTS</div>
           <div className='d-flex col-md-6'>
             {
-              adultCount === 0 ? 
+              adultCount === 1 ? 
                 <ReactSVG className='mr-2' src="/img/icons/minus-circle.svg" /> 
-                :  <ReactSVG className='mr-2' src="/img/icons/minus-circle-active.svg" onClick={() => {
+                :  <ReactSVG className='mr-2' role="button" src="/img/icons/minus-circle-active.svg" onClick={() => {
                       setAdultCount(adultCount - 1)
                     }} />
             }
             <input className='text-right' value={adultCount} style={{width: 40}} onChange={onChangeAdult}></input>
-            <ReactSVG className='ml-2' src="/img/icons/plus-circle.svg" onClick={() => setAdultCount(adultCount + 1)} />
+            <ReactSVG className='ml-2' role="button" src="/img/icons/plus-circle.svg" onClick={() => setAdultCount(adultCount + 1)} />
           </div>
         </div>
         <div className="d-flex row mb-2">
           <div className='col-md-6'>CHILDREN</div>
           <div className='d-flex col-md-6'>
-            <ReactSVG
-             className='mr-2'
-             src="/img/icons/minus-circle.svg" 
-             onClick={() => {
-                if(childrenCount === 0){
-                  return;
-                }
-                setChildrenCount(childrenCount - 1)
-              }}
-             />
+            {
+              childrenCount === 0 ?
+                <ReactSVG className='mr-2' src="/img/icons/minus-circle.svg" />
+                : <ReactSVG
+                    className='mr-2'
+                    role="button"
+                    src="/img/icons/minus-circle-active.svg" 
+                    onClick={() => {
+                      setChildrenCount(childrenCount - 1)
+                    }}
+                  />
+            }
             <input className='text-right' value={childrenCount} style={{width: 40}} onChange={onChangeChildren}></input>
-            <ReactSVG className='ml-2' src="/img/icons/plus-circle.svg" onClick={() => setChildrenCount(childrenCount + 1)} />
+            {
+              childrenCount === 8 ?
+                <ReactSVG className='ml-2' src="/img/icons/plus-circle.svg" />
+                : <ReactSVG 
+                    className='ml-2' 
+                    role="button" 
+                    src="/img/icons/plus-circle.svg" 
+                    onClick={() => setChildrenCount(childrenCount + 1)} />
+            }
           </div>
-          {
-            childrenCount > 0 ? (
-              <div className="d-flex col-md-12">
-                <Select />
-              </div>
-            ): ""
-          }
+          <div className='row w-100'>
+            <ChildrenDiv />
+          </div>
+          
           
         </div>
         <div className="d-flex row mb-2">
           <div className='col-md-6'>INFANTS</div>
           <div className='d-flex col-md-6'>
-            <ReactSVG 
-              className='mr-2'
-              src="/img/icons/minus-circle.svg"
-              onClick={() => {
-                if(infantCount === 0){
-                  return;
-                }
-                setInfantCount(infantCount - 1)
-              }}
-              />
+            {
+              infantCount === 0 ? 
+                <ReactSVG className='mr-2' src="/img/icons/minus-circle.svg" />
+                : <ReactSVG 
+                    className='mr-2'
+                    role="button"
+                    src="/img/icons/minus-circle-active.svg"
+                    onClick={() => {
+                      setInfantCount(infantCount - 1)
+                    }}
+                    />
+            }
+            
             <input className='text-right' value={infantCount} style={{width: 40}} onChange={onChangeInfant}></input>
-            <ReactSVG className='ml-2' src="/img/icons/plus-circle.svg" onClick={() => setInfantCount(infantCount + 1)} />
+            <ReactSVG className='ml-2' role="button" src="/img/icons/plus-circle.svg" onClick={() => setInfantCount(infantCount + 1)} />
           </div>
           {
             infantCount > 0 ? (
-              <div className="d-flex col-md-12">
-                <Select />
+              <div className="d-flex col-4">
+                <Select options={infantAgeOptions}/>
               </div>
             ): ""
           }
