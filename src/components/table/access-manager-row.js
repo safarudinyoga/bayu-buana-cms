8ed92218faff2d30b8f00b0997a75f5303f3fe04
root <hashfirasis91@gmail.com>
2022-05-22 18:06:29 +0700

    progress on user access type

--- src/components/table/access-manager-row.js
+++ src/components/table/access-manager-row.js
@@ -1,49 +1,54 @@
-import React from 'react'
+import { Collapse } from 'bootstrap'
+import React, { useEffect, useState } from 'react'
 import { Form } from "react-bootstrap"
+import { useStateWithCallback } from 'use-state-with-callback'
 
 
 function AccessManagerRow(props) {
-  const { moduleName, category } = props
+  const { moduleName, category, capabilities, moduleId, onChange } = props
+
+  const [capabilityCheckBoxes, setCapabilityCheckBoxes] = useState([])
+  const [allowedModule, setAllowedModule] = useState([])
+
+  const handleChange = (capability, id) => {
+    setAllowedModule([...allowedModule, {
+      id: id,
+      capabilities: {
+        ...capabilities,
+        [capability]: !capabilities[capability]
+      }
+    }])
+  }
+
+  useEffect(() => {
+    const caps = []
+    for(let cap in capabilities) {
+      caps.push(
+        <td>
+          <Form.Check 
+            type="switch"
+            id={`${cap}-${moduleId}`}
+            checked={capabilities[cap]}
+            onChange={() =>
+              handleChange(cap, moduleId)
+            }
+          />
+        </td>
+      )
+    }
+    setCapabilityCheckBoxes(caps)
+  }, [])
+
+  useEffect(() => {
+    console.log(allowedModule)
+  }, [allowedModule])
+  
+
   return (
     <tr>
       <td>{moduleName}</td>
       <td>{category}</td>
-      <td>
-        <Form.Check 
-          type="switch"
-          id={`view-switch-${moduleName}`}
-        />
-      </td>
-      <td>
-        <Form.Check 
-          type="switch"
-          id={`create-switch-${moduleName}`}
-        />
-      </td>
-      <td>
-        <Form.Check 
-          type="switch"
-          id={`delete-switch-${moduleName}`}
-        />
-      </td>
-      <td>
-        <Form.Check 
-          type="switch"
-          id={`edit-switch-${moduleName}`}
-        />
-      </td>
-      <td>
-        <Form.Check 
-          type="switch"
-          id={`mass-update-switch-${moduleName}`}
-        />
-      </td>
-      <td>
-        <Form.Check 
-          type="switch"
-          id={`export-switch-${moduleName}`}
-        />
-      </td>
+      {capabilityCheckBoxes}
     </tr>
   )
 }
