9fc44240cda2eb4054691d1991fc9bdd8c86110d
annisa <annisaayu569@gmail.com>
2022-05-23 14:05:42 +0700

    integration partner currency get by currency_id

--- src/components/table/bb-data-table.js
+++ src/components/table/bb-data-table.js
@@ -163,9 +163,13 @@ class BBDataTable extends Component {
           }).join(" ")
         }
 
+        const targetDataId = module == 'integration-partner-currencies'
+          ? row.currency_id
+          : row.id
+
         return (
           `
-          <a href="javascript:void(0);" data-toggle="tooltip" data-placement="${placement}" class="table-row-action-item ${hideDetail ? "mr-2" : ""}" data-action="edit" data-id="${row.id}" title="Click to edit"><img src="${editIcon}"/></a>
+          <a href="javascript:void(0);" data-toggle="tooltip" data-placement="${placement}" class="table-row-action-item ${hideDetail ? "mr-2" : ""}" data-action="edit" data-id="${targetDataId}" title="Click to edit"><img src="${editIcon}"/></a>
           <a href="javascript:void(0);" data-toggle="tooltip" data-placement="${placement}" class="${hideDetail ? "d-none" : "d-inline"} table-row-action-item" data-action="view" data-id="${row.id}" title="Click to view details"><img src="${showIcon}"/></a>
           <a href="javascript:void(0);" class="${showSwitch ? "d-inline" : "d-none"} custom-switch custom-switch-bb table-row-action-item" data-id="${module == 'employee' ? row.employee_id: row.id}" data-action="update_status" data-status="${row.status}" data-toggle="tooltip" data-placement="${placement}" title="${row.status == 1 ? "Deactivate" : "Activate"}">
             <input type="checkbox" class="custom-control-input check-status-${row.id}" id="customSwitch${row.id}" ${checked} data-action="update_status">
