9623f70cd4edeb1bc079488f645396fbfafaeda4
annisa <annisaayu569@gmail.com>
2022-05-24 15:50:39 +0700

    Integration API edit hotel standard Ancillary fee

--- src/views/standard_ancillary_fee/table/hotel_table.js
+++ src/views/standard_ancillary_fee/table/hotel_table.js
@@ -9,7 +9,7 @@ export default function HotelTable() {
     showAdvancedOptions: false,
     baseRoute: "/master/standard-ancillary-fee/form/hotel-form",
     endpoint: "/master/agent-processing-fee-categories/2",
-    deleteEndpoint: "/master/batch-actions/delete/hotels",
+    deleteEndpoint: "/master/agent-processing-fee-categories/2",
     activationEndpoint: "/master/batch-actions/activate/hotels",
     deactivationEndpoint: "/master/batch-actions/deactivate/hotels",
     hideDetail: true,
@@ -19,9 +19,12 @@ export default function HotelTable() {
         data: "processing_fee_category_name",
       },
     ],
-    emptyTable: "No hotel found",
-    recordName: "hotel_name",
-    module: "standard-ancillary-fee"
+    emptyTable: "No Hotel fees found",
+    recordName: "processing_fee_category_name",
+    module: "standard-ancillary-fee",
+    infoDelete: [
+      {title: "Standard Ancillary Fee", recordName: "processing_fee_category_name"}, 
+    ],
   }) 
 
   return <BBDataTable {...params}  />
