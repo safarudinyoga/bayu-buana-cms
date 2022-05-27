497893c60001006442a1cf41fb714a09bd0c4d4f
sony <sonyfebrian362@gmail.com>
2022-05-27 09:57:51 +0700

    fix standar service

--- src/views/general-setup/standart_service/form/standart-service-form.js.js
+++ src/views/general-setup/standart_service/form/standart-service-form.js.js
@@ -108,7 +108,6 @@ function StandartService(props) {
 
   const validationSchema = Yup.object().shape({
     task_type: Yup.object().required("Task Type is required."),
-    respon_time: Yup.object().required("Task Type is required."),
   })
 
   const onSubmit = async (values, a) => {
