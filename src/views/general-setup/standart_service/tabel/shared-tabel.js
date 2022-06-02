01a63391d2c50c536c0518640a8d3118c781f670
Novian <novian@tog.co.id>
2022-06-02 09:27:11 +0700

    fix standar service

--- src/views/general-setup/standart_service/tabel/shared-tabel.js
+++ src/views/general-setup/standart_service/tabel/shared-tabel.js
@@ -3,7 +3,9 @@ import BBDataTable from "components/table/bb-data-table"
 
 import FormTabel from "../form/standart-service-form.js"
 
-export default function SharedTable() {
+export default function SharedTable(props) {
+  let serviceLevelCode = props.serviceLevelCode;
+  const endpoint = `/master/configurations/standard-services?filters=["service_level.service_level_code","=","${serviceLevelCode}"]`
   let params = {
     isCheckbox: false,
     showAdvancedOptions: false,
@@ -13,7 +15,7 @@ export default function SharedTable() {
     title: "Standard Service",
     titleModal: "Standard Service",
     baseRoute: "/master/standard-services/form",
-    endpoint: "/master/configurations/standard-services",
+    endpoint: endpoint,
     deleteEndpoint:
       "/master/batch-actions/delete/configurations/standard-services",
     activationEndpoint:
@@ -27,11 +29,11 @@ export default function SharedTable() {
       },
       {
         title: "Response Time",
-        data: "service_level_objective.amount",
+        data: "amount",
       },
     ],
     emptyTable: "No Record",
-    recordName: ["task_type.task_type_name", "service_level_objective.amount"],
+    recordName: ["task_type.task_type_name", "amount"],
     btnDownload: ".buttons-csv",
     module: "standard-service",
   }
