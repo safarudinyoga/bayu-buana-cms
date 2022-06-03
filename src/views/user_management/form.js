9c27aea8550bf446cbcaf36707e1c04d16363af5
sony <sonyfebrian362@gmail.com>
2022-06-03 17:06:30 +0700

    fix invoice setting layout

--- src/views/user_management/form.js
+++ src/views/user_management/form.js
@@ -1,92 +1,64 @@
 import React, { useEffect, useState, useRef } from "react"
 import { withRouter, useHistory } from "react-router-dom"
-import { Card, Form, Row, Button, Nav, OverlayTrigger,Popover } from "react-bootstrap"
+import { Card, Form, Row, Button, Nav, Overlay } from "react-bootstrap"
 import { useSnackbar } from "react-simple-snackbar"
 import ModuleAccess from "./module-access"
 import { setUIParams } from "redux/ui-store"
 import SelectAsync from "components/form/select-async"
-import createIcon from "assets/icons/create.svg"
-import { Formik, FastField, Field } from "formik"
+import { Formik, FastField, Form as FormikForm } from "formik"
 import * as Yup from "yup"
-import { ReactSVG } from "react-svg"
-import axios from "axios"
+import useQuery from "lib/query"
 import { useDispatch } from "react-redux"
-import { setAlert, setUIParams } from "redux/ui-store"
+import { setAlert } from "redux/ui-store"
 import Api from "config/api"
-import env from "config/environment"
-import Select from "components/form/select-async"
-import FormInputSelectAjax from "components/form/input-select-ajax"
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
 import "react-dropzone-uploader/dist/styles.css"
+import env from "../../config/environment"
 
 const backUrl = "/master/user-management"
+const endpoint = "/user/user-type-users"
+const options = {
+  position: "bottom-right",
+}
 
-const FlightForm = (props) => {
+const UserManagementForm = (props) => {
   const history = useHistory()
+  const [openSnackbar] = useSnackbar(options)
   let dispatch = useDispatch()
-  const [selectCountry, setSelectCountry] = useState([])
-  const [selectHotelBrand, setSelectHotelBrand] = useState([])
-  const [modalShow, setModalShow] = useState(false)
   let api = new Api()
-  const [modules, setModules] = useState([])
+  const [initialForm, setInitialForm] = useState({
+    employee: "",
+    user_type: "",
+  })
+  let formId = props.match.params.id
+  const [loading, setLoading] = useState(true)
+  const isView = useQuery().get("action") === "view"
+
   const [show, setShow] = useState(false)
   const target = useRef(null)
-
-  //module
-  useEffect(async () => {
-    try {
-      let res = await api.get("/master/menu-links?size=999&sort=sort")
-      const cat = []
-      const mod = []
-      const comp = []
-
-      res.data.items.forEach((data) => {
-        if (!data.parent_link_id) {
-          cat.push(data)
-        } else {
-          mod.push(data)
-        }
-      })
-
-      console.log(cat)
-      let listMod = mod.map((m) => {
-        let catObj = cat.filter((c) => m.parent_link_id.includes(c.id))
-        if (catObj.length > 0) {
-          let catName = catObj[0].menu_link_name
-          m.categoryName = catName
-          comp.push(
-            <AccessManagerRow
-              moduleName={m.menu_link_name}
-              category={m.categoryName}
-            />,
-          )
-        }
-      })
-      // console.log(comp);
-      setModules(comp)
-    } catch (e) {
-      console.log(e)
-      throw e
-    }
-  }, [])
 
   useEffect(async () => {
     let api = new Api()
     let formId = props.match.params.id
-    let docTitle = "Edit Flight Standard Ancillary Fee"
+    console.log({ formId, isView })
+
+    let docTitle = "Edit User"
+
     if (!formId) {
-      docTitle = "Create User Management"
+      docTitle = "Create User"
+    } else if (isView) {
+      docTitle = "User Management Details"
     }
     dispatch(
       setUIParams({
-        title: docTitle,
+        title: isView ? "User Details" : docTitle,
         breadcrumbs: [
           {
-            text: "User & Acccess Management",
+            text: "User & Access Management",
           },
           {
             link: backUrl,
-            text: "User Management",
+            text: "User Management ",
           },
           {
             text: docTitle,
@@ -94,23 +66,45 @@ const FlightForm = (props) => {
         ],
       }),
     )
-  })
-  // Initialize form
-  const initialForm = {
-    // General Information
-    employee: "",
-    user_type: "",
-  }
-
-  const initialFormModalAddMap = {
-    caption: "",
-    image: "",
-  }
+    try {
+      if (formId) {
+        let { data } = await api.get(endpoint + "/" + formId)
+        setInitialForm(data)
+      }
+    } catch (e) {
+      openSnackbar(`error => ${e}`)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
 
   // Schema for yup
   const validationSchema = Yup.object().shape({
-    employee: Yup.object().required("Employee is required."),
-    user_type: Yup.object().required("Employee is required."),
+    employee: Yup.object()
+      .required("Employee is required.")
+      .test(
+        "Unique Employee Name",
+        "User already exists",
+        async (value, ctx) => {
+          let formId = props.match.params.id
+          try {
+            let res = await api.get(
+              `${env.API_URL}/master/employees?filters=["given_name","=","${value}"]`,
+            )
+
+            if (formId) {
+              return (
+                res.data.items.length === 0 || value === initialForm.given_name
+              )
+            } else {
+              return res.data.items.length === 0
+            }
+          } catch (e) {
+            return false
+          }
+        },
+      ),
+    user_type: Yup.object().required("User Type is required."),
   })
 
   const onSubmit = async (values, a) => {
@@ -151,20 +145,14 @@ const FlightForm = (props) => {
       )
     }
   }
-  const popoverBottom =(values) =>{
-    return (
-      <Popover id="popover-positioned-bottom" title="Popover bottom">
-        <ModuleAccess userType={values}/>
-      </Popover>
-    )
-  }
+
   return (
     <>
       <Formik
         initialValues={initialForm}
         validationSchema={validationSchema}
         validateOnChange={false}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {}}
+        onSubmit={onSubmit}
       >
         {({
           values,
@@ -178,7 +166,7 @@ const FlightForm = (props) => {
           setFieldValue,
           setFieldTouched,
         }) => (
-          <Form onSubmit={handleSubmit}>
+          <FormikForm onSubmit={handleSubmit}>
             <Card>
               <Card.Body>
                 <div style={{ padding: "0 2px 2px" }}>
@@ -189,10 +177,11 @@ const FlightForm = (props) => {
                     </Form.Label>
                     <FastField name="employee">
                       {({ field, form }) => (
-                        <div style={{ maxWidth: 200 }}>
+                        <div style={{ width: 150 }}>
                           <SelectAsync
                             {...field}
                             isClearable
+                            isDisabled={values.given_name || isView}
                             url={`master/employees`}
                             fieldName="given_name"
                             onChange={(v) => {
@@ -204,14 +193,14 @@ const FlightForm = (props) => {
                                 ? "is-invalid"
                                 : null
                             }`}
-                            // components={
-                            //   isView
-                            //     ? {
-                            //         DropdownIndicator: () => null,
-                            //         IndicatorSeparator: () => null,
-                            //       }
-                            //     : null
-                            // }
+                            components={
+                              isView
+                                ? {
+                                    DropdownIndicator: () => null,
+                                    IndicatorSeparator: () => null,
+                                  }
+                                : null
+                            }
                           />
                           {form.touched.employee && form.errors.employee && (
                             <Form.Control.Feedback type="invalid">
@@ -223,21 +212,23 @@ const FlightForm = (props) => {
                         </div>
                       )}
                     </FastField>
-
-                    <Form.Label column md={3}>
-                      <Nav.Link href={`/master/employee/form`}>
-                        Create New Employee
-                      </Nav.Link>
-                    </Form.Label>
+                    {!formId ? (
+                      <Form.Label column md={3}>
+                        <Nav.Link href={`/master/employee/form`}>
+                          Create New Employee
+                        </Nav.Link>
+                      </Form.Label>
+                    ) : null}
                   </Form.Group>
 
                   <Form.Group as={Row} className="mb-3">
                     <Form.Label column md={2}>
                       User Type
+                      <span className="form-label-required">*</span>
                     </Form.Label>
                     <FastField name="user_type">
                       {({ field, form }) => (
-                        <div style={{ maxWidth: 200 }}>
+                        <div style={{ width: 150 }}>
                           <SelectAsync
                             {...field}
                             isClearable
@@ -246,20 +237,22 @@ const FlightForm = (props) => {
                             onChange={(v) => {
                               setFieldValue("user_type", v)
                             }}
+                            value={values.user_type}
                             placeholder="Please choose"
                             className={`react-select ${
                               form.touched.user_type && form.errors.user_type
                                 ? "is-invalid"
                                 : null
                             }`}
-                            // components={
-                            //   isView
-                            //     ? {
-                            //         DropdownIndicator: () => null,
-                            //         IndicatorSeparator: () => null,
-                            //       }
-                            //     : null
-                            // }
+                            style={{ witdh: 200 }}
+                            components={
+                              isView
+                                ? {
+                                    DropdownIndicator: () => null,
+                                    IndicatorSeparator: () => null,
+                                  }
+                                : null
+                            }
                           />
                           {form.touched.user_type && form.errors.user_type && (
                             <Form.Control.Feedback type="invalid">
@@ -271,17 +264,18 @@ const FlightForm = (props) => {
                         </div>
                       )}
                     </FastField>
-                    <OverlayTrigger disabled trigger="click" placement="bottom" overlay={popoverBottom(values.user_type)} rootClose>
-                      <Form.Label
-                        column
-                        md={2}
-                        style={{ color: "#3E40AE", marginLeft: "14px" }}
-                      >
-                        View module access list
-                      </Form.Label>
-                    </OverlayTrigger>
+
+                    <Form.Label
+                      column
+                      md={2}
+                      style={{ color: "#3E40AE", marginLeft: "14px" }}
+                      ref={target}
+                      onClick={() => setShow(!show)}
+                    >
+                      View module access list
+                    </Form.Label>
                   </Form.Group>
-                  {/* <Overlay
+                  <Overlay
                     target={target.current}
                     show={show}
                     placement="bottom"
@@ -297,13 +291,10 @@ const FlightForm = (props) => {
                         //   ...props.style,
                         // }}
                       >
-                        <Popover id="popover-positioned-bottom" title="Popover bottom">
-                          <strong>Holy guacamole!</strong> Check this info.
-                        </Popover>
-                        {/* <ModuleAccess userType={values.user_type}/>
+                        <ModuleAccess />
                       </div>
                     )}
-                  </Overlay> */}
+                  </Overlay>
                 </div>
 
                 <div style={{ padding: "0 15px 15px 15px" }}>
@@ -328,11 +319,11 @@ const FlightForm = (props) => {
               </Button>
             </div>
             <div></div>
-          </Form>
+          </FormikForm>
         )}
-      </Formik>
+      </Formik>{" "}
     </>
   )
 }
 
-export default withRouter(FlightForm)
+export default withRouter(UserManagementForm)
