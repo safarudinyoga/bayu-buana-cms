62a0e542e159a5d8bb9894a8939d9299a143c924
annisa <annisaayu569@gmail.com>
2022-06-17 13:18:53 +0700

    layout step review

--- src/views/extranet/book_trip/book_flight/step/add_ons.js
+++ src/views/extranet/book_trip/book_flight/step/add_ons.js
@@ -1,9 +1,23 @@
 import React, {useState, useEffect} from "react"
+import { Col, Row, Card, Form, Button, Alert, Accordion, Modal } from 'react-bootstrap'
+import BBModal from 'components/Modal/bb-modal';
 
-
-const AddOn = () => {
+const AddOn = ({ handleSelectTab }) => {
   return (
-    <p>This feature is not yet available</p>
+    <div className="pt-4">
+      <p>This feature is not yet available</p>
+      <div className='d-flex'>
+        <Button 
+        onClick={(e) => { 
+          handleSelectTab("review")
+        }}
+        className="btn-flight-select mr-3"
+        >
+            continue booking
+        </Button>
+        <Button variant="secondary" onClick={() => handleSelectTab('select-seats')}>Back</Button>
+      </div>
+    </div>
   )
 }
 
