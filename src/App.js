a274b59baa6e71841cc9302bda09fa7596704a58
Safarudin Yoga <safarudinyoga@gmail.com>
2022-06-22 05:44:19 +0700

    :zap: remove unused

--- src/App.js
+++ src/App.js
@@ -831,11 +831,6 @@ const DashboardRoutes = () => {
           <CorporateUserProfile />
         </Route>
 
-        {/* Corporate Branch Office */}
-        {/* <Route exact path="/master/branch-offices">
-          <OfficeTable />
-        </Route> */}
-
         {/* Corporate User Management */}
         <Route exact path="/master/corporate-user-management">
           <CorporateUserManagementTable />
