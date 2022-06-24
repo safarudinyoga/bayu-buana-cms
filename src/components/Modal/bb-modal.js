b9ee8c46ea78fc18b031977c98c5213c83c40684
safarudinyoga <safarudinyoga@gmail.com>
2022-06-24 20:56:13 +0700

    :zap: fixing layout modal hit on other

--- src/components/Modal/bb-modal.js
+++ src/components/Modal/bb-modal.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Modal, ModalBody } from "react-bootstrap"
 import './bb-modal.css'
 
-const ModalCreate = ({show, onClick, modalContent, modalTitle, modalSize, scrollable=false}) => {
+const ModalCreate = ({show, onClick, modalContent, modalTitle, modalSize, scrollable=false, className }) => {
 	const Content = modalContent
 	return (
 		<Modal
@@ -13,6 +13,7 @@ const ModalCreate = ({show, onClick, modalContent, modalTitle, modalSize, scroll
 			centered
 			dialogClassName={!modalSize ? "bb-modal-dialog" : ""}
 			scrollable={scrollable}
+			className={className}
 		>
 		<Modal.Header closeButton className="bb-modal-header">
 		</Modal.Header>
