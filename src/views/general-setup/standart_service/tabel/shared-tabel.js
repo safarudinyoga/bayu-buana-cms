193051783826b6fa4823bf46fdc004eeba235bfe
sony <sonyfebrian362@gmail.com>
2022-06-27 18:47:26 +0700

    fix

--- src/views/general-setup/standart_service/tabel/shared-tabel.js
+++ src/views/general-setup/standart_service/tabel/shared-tabel.js
@@ -1,17 +1,42 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import BBDataTable from "components/table/bb-data-table"
-
+import _ from "lodash"
+import Api from "config/api"
 import FormTabel from "../form/standart-service-form.js"
 
+const endpoint = "/master/configurations/email-senders"
 export default function SharedTable(props) {
-  let serviceLevelCode = props.serviceLevelCode;
+  let serviceLevelCode = props.serviceLevelCode
+  const API = new Api()
+  const [initialForm, setInitialForm] = useState({
+    message_type: "",
+  })
+
+  useEffect(async () => {
+    try {
+      let { data } = await API.get(endpoint)
+
+      console.log("tes", data)
+      setInitialForm({
+        ...initialForm,
+        message_type: _.isEmpty(data.message_type)
+          ? ""
+          : {
+              value: data.message_type.id,
+              label: data.message_type.message_type_name,
+            },
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }, [])
   const endpoint = `/master/configurations/standard-services`
   const extraFilter = `["service_level.service_level_code","=","${serviceLevelCode}"]`
   let params = {
     isCheckbox: false,
     showAdvancedOptions: false,
     createOnModal: true,
-    hideDetail:true,
+    hideDetail: true,
     showHistory: false,
     isHideDownloadLogo: true,
     title: "Standard Service",
@@ -34,26 +59,26 @@ export default function SharedTable(props) {
         title: "Response Time",
         data: "amount",
         render: (data) => {
-          let day = Math.round(data/1440)
-          let hour = Math.round((data % 1440)/60)
+          let day = Math.floor(data / 1440)
+          let hour = Math.floor((data % 1440) / 60)
           let minute = (data % 1440) % 60
-          
+
           let days = ""
-          if(day > 0){
+          if (day > 0) {
             days = day + (day > 1 ? " Days" : " Day")
           }
 
           let hours = ""
-          if(hour > 0){
+          if (hour > 0) {
             hours = hour + (hour > 1 ? " Hours" : " Hour")
           }
 
           let minutes = ""
-          if(minute > 0){
+          if (minute > 0) {
             minutes = minute + (minute > 1 ? " Minutes" : " Minute")
           }
           return days + " " + hours + " " + minutes
-        }
+        },
       },
     ],
     emptyTable: "No Record",
@@ -61,8 +86,14 @@ export default function SharedTable(props) {
     module: "standard-service",
     showInfoDelete: true,
     infoDelete: [
-      {title: "Task Type", recordName: "task_type_name"}
+      { title: `Task Type`, recordName: "task_type.task_type_name" },
     ],
   }
-  return <BBDataTable {...params} modalContent={FormTabel} serviceLevelCode={serviceLevelCode}/>
+  return (
+    <BBDataTable
+      {...params}
+      modalContent={FormTabel}
+      serviceLevelCode={serviceLevelCode}
+    />
+  )
 }
