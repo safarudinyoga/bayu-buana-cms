8a430f375a235f5d641b97b40909bd90d9352126
Ezra <ezraip98@gmail.com>
2022-06-27 22:16:15 +0700

    fix: icon travel consultant, team assignment

--- src/components/card/add-or-remove-list.js
+++ src/components/card/add-or-remove-list.js
@@ -6,6 +6,8 @@ import arrowLeft from "assets/icons/arrow-left.svg"
 import arrowRight from "assets/icons/arrow-right.svg"
 import xCircle from "assets/icons/x-circle.svg"
 import flightTicket from "assets/icons/flight-ticket.svg"
+import peopleCheck from "assets/icons/people-check.svg"
+import ticketDetailed from "assets/icons/ticket-detailed.svg"
 import FormInputSelectAjax from "components/form/input-select-ajax"
 import FormikControl from "components/formik/formikControl"
 import "./add-or-remove-list.css"
@@ -114,8 +116,8 @@ const AddOrRemoveList = ({
                 >
                   {canRemoveIndex ? (
                     <div className="w-100 d-flex justify-content-between align-items-center">
-                      {item.given_name} {item.middle_name} {item.surname} (
-                      {item?.office_name})
+                      {item.given_name} {item.middle_name} {item.surname}{" "}
+                      {item.office_name && `(${item?.office_name})`}
                       <FieldArray
                         name="employee"
                         render={(arr) => (
@@ -166,7 +168,7 @@ const AddOrRemoveList = ({
                             // }
                             className="add-remove-cb"
                           />
-                          <img src={flightTicket} alt="flight-ticket" />
+                          <img src={onModal ? peopleCheck : ticketDetailed} alt="flight-ticket" />
                         </label>
                       </div>
                       {onModal && (
@@ -188,7 +190,7 @@ const AddOrRemoveList = ({
                               }
                               className="add-remove-cb"
                             />
-                            <img src={flightTicket} alt="flight-ticket" />
+                            <img src={ticketDetailed} alt="flight-ticket" />
                           </label>
                         </div>
                       )}
@@ -231,10 +233,10 @@ const AddOrRemoveList = ({
                 className="label-flight-ticket"
                 style={{ backgroundColor: "#027F71", padding: "1px 8px 3px" }}
               >
-                <img src={flightTicket} alt="flight-ticket" />
+                <img src={peopleCheck} alt="flight-ticket" />
               </div>
               <span style={{ fontSize: "12px", paddingLeft: "8px" }}>
-                Click to assign/remove assignment as leader.
+                Click to allow/ forbid travel consultant to issue flight ticket.
               </span>
             </div>
             {onModal && (
@@ -243,7 +245,7 @@ const AddOrRemoveList = ({
                   className="label-flight-ticket"
                   style={{ backgroundColor: "#027F71", padding: "1px 8px 3px" }}
                 >
-                  <img src={flightTicket} alt="flight-ticket" />
+                  <img src={ticketDetailed} alt="flight-ticket" />
                 </div>
                 <span style={{ fontSize: "12px", paddingLeft: "8px" }}>
                   Click to assign/forbid travel consultant to issue flight
@@ -402,7 +404,9 @@ const AddOrRemoveList = ({
                     </svg>
                     <div className="w-100 d-flex justify-content-between">
                       {item.given_name} {item.middle_name} {item.surname}
-                      <span>({item?.office_name})</span>
+                      <span>
+                        {item.office_name && `(${item?.office_name})`}
+                      </span>
                     </div>
                   </div>
                 </li>
