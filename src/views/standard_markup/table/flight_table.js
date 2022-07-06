c16ddcee2c461fed60e10ad05676c678686d67f3
Ezra <ezraip98@gmail.com>
2022-07-06 11:00:32 +0700

    fix: edit form on flight & hotel standard markup

--- src/views/standard_markup/table/flight_table.js
+++ src/views/standard_markup/table/flight_table.js
@@ -24,18 +24,24 @@ export default function FlightTable() {
         title: "Domestic Mark Up",
         data: "domestic_flight_markup",
         render: (val) => {
-          if (val.charge_type_id === "c93288b6-29d3-4e20-aa83-5ee6261f64ff") {
-            return `IDR ${val.amount}/Ticket`
-          } else if (
-            val.charge_type_id === "de03bf84-4bd8-4cdf-9348-00246f04bcad"
-          ) {
-            return `IDR ${val.amount}/Person`
-          } else if (
-            val.charge_type_id === "5123b121-4f6a-4871-bef1-65408d663e19"
-          ) {
-            return `IDR ${val.amount}/Transaction`
+          if (val.amount !== 0) {
+            if (val.charge_type_id === "c93288b6-29d3-4e20-aa83-5ee6261f64ff") {
+              return `IDR ${val.amount}/Ticket`
+            } else if (
+              val.charge_type_id === "de03bf84-4bd8-4cdf-9348-00246f04bcad"
+            ) {
+              return `IDR ${val.amount}/Person`
+            } else if (
+              val.charge_type_id === "5123b121-4f6a-4871-bef1-65408d663e19"
+            ) {
+              return `IDR ${val.amount}/Transaction`
+            }
           } else {
-            return `${val.percent}% Include Tax `
+            if (val.is_tax_inclusive) {
+              return `${val.percent}% Include Tax `
+            } else {
+              return `${val.percent}% Not Include Tax `
+            }
           }
         },
       },
@@ -43,18 +49,24 @@ export default function FlightTable() {
         title: "International Mark Up",
         data: "international_flight_markup",
         render: (val) => {
-          if (val.charge_type_id === "c93288b6-29d3-4e20-aa83-5ee6261f64ff") {
-            return `IDR ${val.amount}/Ticket`
-          } else if (
-            val.charge_type_id === "de03bf84-4bd8-4cdf-9348-00246f04bcad"
-          ) {
-            return `IDR ${val.amount}/Person`
-          } else if (
-            val.charge_type_id === "5123b121-4f6a-4871-bef1-65408d663e19"
-          ) {
-            return `IDR ${val.amount}/Transaction`
+          if (val.amount !== 0) {
+            if (val.charge_type_id === "c93288b6-29d3-4e20-aa83-5ee6261f64ff") {
+              return `IDR ${val.amount}/Ticket`
+            } else if (
+              val.charge_type_id === "de03bf84-4bd8-4cdf-9348-00246f04bcad"
+            ) {
+              return `IDR ${val.amount}/Person`
+            } else if (
+              val.charge_type_id === "5123b121-4f6a-4871-bef1-65408d663e19"
+            ) {
+              return `IDR ${val.amount}/Transaction`
+            }
           } else {
-            return `${val.percent}% Include Tax `
+            if (val.is_tax_inclusive) {
+              return `${val.percent}% Include Tax `
+            } else {
+              return `${val.percent}% Not Include Tax `
+            }
           }
         },
       },
